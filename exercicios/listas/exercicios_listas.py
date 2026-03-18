1ff2abbaab9f352eb01acca0be76a6a4983ee8cb
Caio <caiodesouza.cds@gmail.com>
2026-03-18 17:30:23 -0300

    Feito exercicios de strings

---
+++ exercicios/listas/exercicios_listas.py
@@ -0,0 +1,21 @@
+#EXERCICIOS DE LISTAS
+
+"""
+1) Adicione, de forma programática, o pib da Turquia na lista `pib`. Pegue este valor [aqui](https://pt.wikipedia.org/wiki/Economia_de_Angola)).
+"""
+
+pib = [
+    ["china", 23120],
+    ["usa", 19360],
+    ["india", 9447],
+    ["japan", 5405],
+    ["germany", 4150],
+    ["russia", 4000],
+    ["indonesia", 3243],
+    ["brazil", 3219]
+]
+
+pib.append(["angola", 9463])
+
+
+#--------------------------------------------------------------------------------------------
